71b5089887abfee6b0c096bf35141a061acbf70c
Asif Mohammed <asif@bareminimum.company>
2024-03-08 00:22:15 +0400

    ref: update works path

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,9 +16,15 @@ const servicesCollection = defineCollection({
 
 const worksCollection = defineCollection({
 	type: 'content',
-	schema: z.object({
-		title: z.string(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			cover: image().refine((img) => img.width >= 1080, {
+				message: 'Cover image must be at least 1080 pixels wide!',
+			}),
+			coverAlt: z.string(),
+		}),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
